9a2cb1d778516304d640eb761e4dab9ba966e682
r.hernadev <raymondhr12@gmail.com>
2025-02-27 23:30:22 -0500

    fix: change in main.ts to get to front end

--- amp-csr-backend/src/main.ts
+++ amp-csr-backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.PORT || '*' });
+  app.enableCors({
+    origin: 'https://amp-csr-frontend.vercel.app/', // front end url
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
