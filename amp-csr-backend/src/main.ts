a6107bc7efdd79546ec9a24f86ba37f381c47a3e
r.hernadev <raymondhr12@gmail.com>
2025-02-27 23:31:55 -0500

    refactor: change in main.ts again

--- amp-csr-backend/src/main.ts
+++ amp-csr-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://amp-csr-frontend.vercel.app/', // front end url
+    origin: 'amp-csr-frontend-qbqg3xzzc-rayherna18s-projects.vercel.app', // front end url
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
