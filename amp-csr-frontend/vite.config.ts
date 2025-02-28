4514fce8b7d4706e72ce26a6a7131635ba6ac566
r.hernadev <raymondhr12@gmail.com>
2025-02-27 23:06:51 -0500

    backend url added to vite config

--- amp-csr-frontend/vite.config.ts
+++ amp-csr-frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/users': 'http://localhost:3000', // Adjust this as needed, depending on where your backend is running
+      '/users': 'amp-csr-backend-hbklmk6t6-rayherna18s-projects.vercel.app', // Adjust this as needed, depending on where your backend is running
     },
   },
   
