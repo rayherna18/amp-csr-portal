790756fa41758a0ca1d04484a2f3ea9fe30d1030
r.hernadev <raymondhr12@gmail.com>
2025-02-27 23:26:13 -0500

    fix: adjusting port for deployment

--- amp-csr-backend/src/main.ts
+++ amp-csr-backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.FRONTEND_URL || '*' });
+  app.enableCors({ origin: process.env.PORT || '*' });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
