8015ffa27ab978987a09fc17f8c5bc7165b8144a
r.hernadev <raymondhr12@gmail.com>
2025-02-27 23:54:38 -0500

    axios change final attempt

--- amp-csr-frontend/src/api/axiosInstance.ts
+++ amp-csr-frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.VITE_BACKEND_URL || 'http://localhost:3000', // Use Vite environment variable
+    baseURL: import.meta.env.VITE_API_BASE_URL, // Use Vite environment variable
     headers: {
       "Content-Type": "application/json",
     },
