448042d0478ee5218c6be807c6108234d87d9d45
r.hernadev <raymondhr12@gmail.com>
2025-02-28 00:37:08 -0500

    docs: added demo gifs and images

--- amp-csr-backend/src/main.ts
+++ amp-csr-backend/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'amp-csr-frontend-qbqg3xzzc-rayherna18s-projects.vercel.app', // front end url
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: process.env.FRONTEND_URL || '*', 
   });
   await app.listen(process.env.PORT ?? 3000);
 }
