09b29ef732a04772ff1f89d14ed6fb1915d0d02c
r.hernadev <raymondhr12@gmail.com>
2025-02-27 22:31:45 -0500

    changes to frontend and backend to deploy correctly

--- amp-csr-frontend/src/api/axiosInstance.ts
+++ amp-csr-frontend/src/api/axiosInstance.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.NODE_ENV === 'production' 
+      ? 'amp-csr-backend-62ypdes0f-rayherna18s-projects.vercel.app' // production
+      : 'http://localhost:3000',  // local dev
     headers: {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
     },
-    });
+  });
 
 export default api;
--- amp-csr-frontend/vite.config.ts
+++ amp-csr-frontend/vite.config.ts
@@ -6,9 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      '/users': 'http://localhost:3000',
-    },
+    proxy: process.env.NODE_ENV === 'production'
+      ? {} // No proxy in production
+      : {
+          '/users': 'http://localhost:3000',
+        },
   },
   
 })
